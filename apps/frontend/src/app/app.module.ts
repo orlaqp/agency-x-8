9e1b3edf9b084631ce34f12982548bfdb17cf5a2
Orlando Quero <orlaqp@gmail.com>
2020-01-26 09:13:01 -0500

    added angular material

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +20,8 @@ import { RouterModule } from '@angular/router';
         }
       ],
       { initialNavigation: 'enabled' }
-    )
+    ),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
