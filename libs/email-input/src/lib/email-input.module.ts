b46174cca159db6114745dc82cc3b004adbc5f51
Orlando Quero <orlaqp@gmail.com>
2020-02-23 18:41:26 -0500

    styled our email control

--- libs/email-input/src/lib/email-input.module.ts
+++ libs/email-input/src/lib/email-input.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmailInputComponent } from './email-input/email-input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AngularMaterialModule } from '@agency-x/angular-material';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, AngularMaterialModule],
   exports:[EmailInputComponent],
   declarations: [EmailInputComponent]
 })
