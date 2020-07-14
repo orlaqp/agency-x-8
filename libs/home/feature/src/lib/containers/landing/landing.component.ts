e89aba2e5416a0c93e7af3f5613e78f180a51c6c
Orlando Quero <orlaqp@binaryways.com>
2020-07-13 22:15:43 -0400

    small laning page claening

--- libs/home/feature/src/lib/containers/landing/landing.component.ts
+++ libs/home/feature/src/lib/containers/landing/landing.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Select } from '@ngxs/store';
 import { AuthState, OidcUser } from '@agency-x/auth/data-access';
-import { Observable, Subscribable, Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 
@@ -26,14 +26,13 @@ export class LandingComponent implements OnInit, OnDestroy {
     
     ngOnInit() {
         this.user$.subscribe(u => {
-            debugger;
             if (u) this.router.navigateByUrl('/home');
         })
     }
 
     ngOnDestroy(): void {
         if (!this.sub) return;
-        
+
         this.sub.unsubscribe();
     }
 
